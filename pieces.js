ded0a364c7a4782ef990f4e5fcdc585c0851326f
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-04-19 16:10:32 +0200

    ajout boutons description et filtre

--- pieces.js
+++ pieces.js
@@ -2,22 +2,82 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[0];
-
+// Création des balises
+for (let i = 0 ; i < pieces.length ; i++) {
+const article = pieces[i];
+  // Récupération de l'élément du DOM qui accueillera les fiches
+  const sectionFiches = document.querySelector (".fiches")
+   // Création d’une balise dédiée à une pièce automobile
+   const pieceElement = document.createElement("article");
+    // Création des balises 
 const imageElement = document.createElement ("img");
-imageElement.src = article.image;
+    imageElement.src = article.image;
+
 const nomElement = document.createElement ("h2");
-nomElement.innerText = article.nom;
+    nomElement.innerText = article.nom;
+
 const prixElement = document.createElement ("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
 const categorieElement = document.createElement ("p");
-categorieElement.innerText = article.categorie;
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-const sectionFiches = document.querySelector (".fiches")
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
 
+const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+ 
+    // On rattache la balise article a la section Fiches
+ sectionFiches.appendChild(pieceElement);
+  // On rattache l’image et les détails à pieceElement (la balise article)
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(stockElement);
 
+}
+
+// Gestion des boutons
+const boutonTrier = document.querySelector(".btn-trier");
+
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from (pieces)
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+const boutonTrierDécroissant = document.querySelector(".btn-trier-decroissant");
+
+boutonTrierDécroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from (pieces)
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix ;
+    });
+    console.log(piecesOrdonnees);
+});
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click" , function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+    console.log (piecesFiltrees)
+
+});
+
+const boutonDescription = document.querySelector(".btn-description");
+
+boutonDescription.addEventListener("click" , function() {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description 
+    });
+ 
+    console.log (piecesFiltrees)
+})
 
